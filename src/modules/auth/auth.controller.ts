772a0442125656bbe2efc87ad94da7eb049f4d24
Ysaac Andujar Binet <ysaac@codika.net>
2024-05-11 12:39:00 -0400

    public decorator

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import { AuthService } from './auth.service';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { LoginDto } from './dto/login.dto';
 import { SigninDto } from './dto/signing.dto';
+import { Public } from 'src/decorators/public.decorator';
 
 @Controller('auth')
 @ApiTags('Auth')
@@ -10,11 +11,13 @@ import { SigninDto } from './dto/signing.dto';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
   
+  @Public()
   @Post('login')
   async login(@Body() loginDto: LoginDto) {
     return await this.authService.login(loginDto)
   }
   
+  @Public()
   @Post('signin')
   async signin(@Body() signinDto: SigninDto) {
     return await this.authService.signin(signinDto)
