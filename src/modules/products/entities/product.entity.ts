294d142165d20340cac256ea366ad68b3a2c5eb8
Ysaac Andujar Binet <ysaac@codika.net>
2024-05-10 21:37:58 -0400

    product entity

--- src/modules/products/entities/product.entity.ts
+++ src/modules/products/entities/product.entity.ts
@@ -1,1 +1,41 @@
-export class Product {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Product {
+
+    @PrimaryGeneratedColumn()
+    id: string;
+    
+    @Column('text', {
+        unique: true,
+    })
+    handle: string;
+    
+    @Column('text')
+    title: string;
+    
+    @Column('text')
+    description: string;
+    
+    @Column('text')
+    sku: string;
+    
+    @Column('float')
+    grams: number;
+    
+    @Column('int')
+    stock: number;
+    
+    @Column('float')
+    price: number;
+    
+    @Column('float')
+    comparePrice: number;
+    
+    @Column('text',{nullable: true,})
+    barcode: string;
+
+    // @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    // @Column('Date')
+    // createdAt: Date;
+}
